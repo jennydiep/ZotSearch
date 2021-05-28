6723878b949d0e8eec3ac03e8b6492ee1b01fca6
jennydiep <jtdiep@uci.edu>
2021-05-28 16:44:14 -0700

    implemented boolean search with tf-idf weighting

--- index.py
+++ index.py
@@ -7,59 +7,90 @@ from bs4 import BeautifulSoup
 import json
 import time
 import pickle
+import math
+import gc
+from operator import attrgetter
+
+# files names:
+urls_foldername = './developer'
+index_filename = 'data/index.txt'
+docIds_filename = 'data/docIds.txt'
+alpha_filename = 'data/alphaIndex.txt'
+merged_filename = "data/merged.txt"
 
 class Posting:
     def __init__(self, docId, wordFreq):
         self.docId = docId
         self.wordFreq = wordFreq
+    def __repr__(self):
+        # return repr("Posting: docId=" + self.docId + " wordFreq=" + self.wordFreq)
+        return repr( "(" + self.docId + ", " + self.wordFreq + ")")
+
+class Item:
+    def __init__(self, token, postings):
+        self.token = token
+        self.postings = postings
+
+    def __lt__(self, other):
+        return self.token < other.token
+    def __gt__(self, other):
+        return self.token > other.token
 
 def buildIndex(directory):
     '''
     function that takes in directory of files that contain set of text documents
     returns inverted index
     '''
-    doc_count = 0
     index = dict()
-    docId = 0                                                       # docId starts at 0
-    docIds = dict()                                                 # hash of docIds to urls
+    docId = 0                                                               # docId starts at 0
+    docIds = dict()                                                         # hash of docIds to urls
+    count = 1
+    num_indices_saved = 0
 
     for root, subdirectories, files in os.walk(directory):
-        for filename in files:                                                  # for each document
+        for filename in files:                                              # for each document
             filepath = os.path.join(root, filename)
-            doc_count += 1
             docId += 1                                                      # increase docId
             with open(filepath, "r", encoding="utf8") as f:
                 data = json.load(f)
                 url = data['url']
-                if url not in docIds.values():
+                if url not in docIds.values():                              # if docid not in dict then add new url docid to dict
                     docIds[docId] = url
-            tokens = tokenize(filepath)
-            wordFreq = computeWordFrequencies(tokens)            # returns hashtable of words and frequency of that word in the document
+            tokens = tokenize(data['content'])
+            wordFreq = computeWordFrequencies(tokens)                       # returns hashtable of words and frequency of that word in the document
             for token in wordFreq:
                 posting = Posting(docId, wordFreq[token])
                 if token not in index:                                      # if token is not in index create new list of posting
                     index[token] = []        
                 index[token].append(posting)                                # else just append to current list of postings
-            # print(f"{doc_count}")
+            
+            # save to file (3)
+            if (count % math.ceil(file_count/3) == 0) or (count == file_count):                               # if count is size 1/3 of total number of docs, save partial index to file OR last doc
+                num_indices_saved += 1
+                partial_index_name = "data/index_" + str(num_indices_saved) + '.txt'
+                # print(index)
+                saveAndSortIndexToFile(index, partial_index_name)
+                del index                                          # clear index after saving
+                gc.collect()
+                index = dict()
+                print("indexing to file '" + partial_index_name + "'")
+            count += 1
     return index, docIds
 
-
-def tokenize(filename: str) -> list:
+# HELPER FUNCTIONS FOR INDEX
+def tokenize(text: str) -> list:
     ''' 
-    reads text file from filename and returns a list of tokens from that file
+    reads text and returns a list of tokens from that text
     '''
     tokens = []                 
-            
 
-    with open(filename, "r", encoding="utf8") as f:
-        data = json.load(f)
-        soup = BeautifulSoup(data['content'], 'html.parser')
+    soup = BeautifulSoup(text, 'html.parser')
 
-        for line in soup.get_text().split('\n'):
-            line = line.lower()
-            line = re.sub("[^a-z0-9\s-]", "", line)  # to remove all non alphanumeric characters
-            temp = re.findall("[a-z0-9]+", line)
-            tokens += temp                          # add result
+    for line in soup.get_text().split('\n'):
+        line = line.lower()
+        line = re.sub("[^a-z0-9\s-]", "", line)  # to remove all non alphanumeric characters
+        temp = re.findall("[a-z0-9]+", line)
+        tokens += temp                          # add result
 
     return tokens
 
@@ -76,34 +107,164 @@ def computeWordFrequencies(tokens: list) -> dict:
             word_freq[token] += 1        # else if already in map increase counter   O(1)
     return word_freq                     # O(1)
 
-def saveIndexToFile(index: dict):
-    '''
-    takes in inverted index (hashtable) and saves to disk
-    '''
-    with open("index.txt", 'a') as f:
-        for token in index:
-            string = f"{token} ["
 
-            for posting in index[token]:
-                string += f"({posting.docId}, {posting.wordFreq}), "
-            string += "]\n"
+# HELPER FUNCTIONS
+def count_files(directory):
+    count = 0
+    for root, subdirectories, files in os.walk(directory):
+        for file in files:
+            count += 1
+    return count
+
+def resetFiles(files):
+    '''overwrite files in list to be empty'''
+    for filename in files:
+        with open(filename, "w") as f:
+            f.write("")
+
+def saveAndSortIndexToFile(index: list, filename: str):
+    '''
+    takes in inverted index that's sorted and saves to disk
+    '''
+    sortedindex = sorted(index.keys(), key=lambda item: item.lower())
+    with open(filename, 'w') as f:
+        for item in sortedindex:
+            string = f"{item}  "                                        # item[0] = token
+            for posting in index[item]:                                        # item[1] is list of postings
+                string += f" {posting.docId} {posting.wordFreq}  "
+            string += "\n"
 
             f.write(string)
     return
 
+def twoWayMerge(file1, file2, outputfile):
+    ''' takes in two files and merges into one big file '''
+    f1 = open(file1, 'r')
+    f2 = open(file2, 'r')
+
+    # files_to_read = [f1, f2]
+
+    line1 = f1.readline()
+    line2 = f2.readline()
+
+    with open(outputfile, "a+") as f:
+        while line1 != '' and line2 != '':               # while files aren't at EOF
+            item1 = parseline(line1)                    # get token and postings from line
+            item2 = parseline(line2)
+
+            if line1 == '':
+                string = createString(item2)            
+                f.write(string)
+                line2 = f2.readline()
+            elif line2 == '':
+                string = createString(item1)            
+                f.write(string)
+                line1 = f1.readline()
+            
+            # find smallest token and add to new file, read next line 
+            elif (item1 < item2):
+                string = createString(item1)            
+                f.write(string)
+                line1 = f1.readline()
+            elif (item2 < item1):
+                string = createString(item2)
+                f.write(string)
+                line2 = f2.readline()
+            # else if both are same token merge postings and add to file, read next line of both files
+            elif (item1.token == item2.token):
+                temp = item1.postings + item2.postings
+                newItem = Item(item1.token, temp)
+                string = createString(newItem)
+                f.write(string)
+
+                line1 = f1.readline()
+                line2 = f2.readline()
+
+    f1.close()
+    f2.close()
+
+def createString(item):
+    ''' converts item to string '''
+    string = f"{item.token}  "                                        # item[0] = token
+    for posting in item.postings:                                        # item[1] is list of postings
+        string += f" {posting.docId} {posting.wordFreq}  "
+    string += "\n"
+    return string
+
+def parseline(line):
+    ''' takes line that was read from a file and convert to an item '''
+    elements = line.split()
+    token = elements[0]
+    postings = convertToPostings(elements[1:])
+    item = Item(token, postings)
+
+    return item
+
+def convertToPostings(array: list):
+    ''' takes in list of elements and converts them into pairs of postings'''
+    postings = []
+    for i in range(0, len(array), 2):
+        postings.append(Posting(array[i], array[i+1]))
+    return postings
+
+
+# make function to index the merged index by alphabet
+def indexIndexByAlphabet(filename: str):
+    index = dict()
+    letter = '0'
+    index[letter] = 0
+    with open(filename, 'r') as f:
+        line = f.readline()
+        while line:
+            if letter != line[0]:                   # on next letter, add to hash table
+                letter = line[0]
+                index[letter] = f.tell()
+            line = f.readline()
+    return index
+
+
+
 if __name__ == "__main__":
+    # reset text files to be empty
+    resetFiles([docIds_filename, "data/temp.txt", merged_filename]) # don't know why but when emptying a file it's faster to write to an empty file than just overwriting the file.
+    
+    # start timer
     t0 = time.time()
-    index, docIds = buildIndex("./analyst")
-    saveIndexToFile(index)
-    pickle.dump(docIds, open('docIds.txt', 'wb'))
-    pickle.dump(index, open('indexfile.txt', 'wb'))
+
+    file_count = count_files(urls_foldername)
+    print(file_count)
+
+    # build index
+    index, docIds = buildIndex(urls_foldername)
+
+    # merge partial indices
+    twoWayMerge("data/index_1.txt", "data/index_2.txt", "data/temp.txt")
+    twoWayMerge("data/temp.txt", "data/index_3.txt", merged_filename)
+    print("partial indices merged.")
+
+    # index the index by alphabet
+    alphaIndex = indexIndexByAlphabet(merged_filename)
+    indexIndexByAlphabet(merged_filename)
+
+    # dump docIds to a file
+    pickle.dump(docIds, open(docIds_filename , 'wb'))
+    print("docIds are saved.")
+
+    # dump alpha index to a file
+    print(alphaIndex)
+    pickle.dump(alphaIndex, open(alpha_filename , 'wb'))
+    print("index of index by alphabet is saved.")
+
+    # get end time
     t1 = time.time()
 
     total_time = (t1-t0)/60
     print(f"program took {int(total_time * 100)/100} minutes.")
+
 '''
 references:
 
 https://www.kite.com/python/answers/how-to-list-all-subdirectories-and-files-in-a-given-directory-in-python
 https://newbedev.com/how-to-iterate-over-files-in-a-given-directory
+https://stackoverflow.com/questions/1316767/how-can-i-explicitly-free-memory-in-python
 '''
--- search.py
+++ search.py
@@ -1,5 +1,25 @@
 import pickle
-from index import Posting
+from index import Posting, resetFiles
+from index import Item
+# from index import convertToPostings
+import time
+from index import count_files
+from index import urls_foldername
+import math
+
+merged_filename = 'data/merged.txt'
+
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 
+                'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 
+                'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 
+                'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 
+                'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 
+                'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 
+                'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 
+                'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 
+                'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 
+                'won', "won't", 'wouldn', "wouldn't"]
+
 
 # def documentAtATimeRetrieval(query, index, f, g, k):
 #     '''
@@ -23,41 +43,126 @@ from index import Posting
 
 # [(1, 1), (12, 1), (273, 1), (1040, 1), (1840, 1), ]
 
-def termAtATimeRetrieval(query, index, k):
+def termAtATimeRetrieval(query, indexfilename, alphaIndex, k):
     '''
     takes in inputs and returns top k documents that match the query]
     '''
     accumulators = dict()                   # structure -> docId : score
     inverted_lists = []
     results = [] # TODO make priority queue
+    list_of_docIDs = []
 
-    query_terms = query.strip('\n').split(' ')             # split query terms
+    indexfile = open(indexfilename, 'r')
+
+    query_terms = query.strip('\n').split()             # split query terms
+    [word for word in query_terms if word not in stopwords]
     for term in query_terms:
-        inverted_lists.append(index[term])      # sort inverted lists of each term from query
-    for list in inverted_lists:
-        for posting in list:                    # get postings from inverted lists
-            docId = posting.docId
-            wordfreq = posting.wordFreq
+        li, ids = findTermInFile(term, indexfile, alphaIndex)
+        inverted_lists.append(li)                           # store inverted lists of each term from query (only scoring documents that contain query terms)
+        list_of_docIDs.append(ids)
+    list = intersectPostings(inverted_lists, list_of_docIDs)
+    # print(inverted_lists)
+    for postings in list:
+        for key, value in postings.items():                    # get postings from inverted lists
+    # for posting in list: 
+            docId = key
+            wordfreq = value
+            tfidf = (1 + math.log(int(wordfreq))) * math.log(file_count/len(postings))
             if docId not in accumulators:       # create or add scores to documents
-                accumulators[docId] = wordfreq
+                accumulators[docId] = tfidf
             else:
-                accumulators[docId] += wordfreq
+                accumulators[docId] += tfidf
     
+    indexfile.close()
     # get top k scores
     results =  sorted(accumulators.items(), key=lambda posting: posting[1], reverse=True)
-    return results[0:k]
+    return results[0:min(len(results), k)]
+
+# HELPER FUNCTIONS
+def findTermInFile(term: str, indexfile, alphaIndex: dict()):
+    ''' finds term in merged index file'''
+    t0 = time.time()
+
+    letter = term[0]
+    start = alphaIndex[letter]
+    
+    indexfile.seek(start)
+    while True:
+        line = indexfile.readline().split()
+        
+        token = line[0]
+
+        if token == term:
+            postings, docIds = convertToPostings(line[1:])
+            # print('found')
+            t1 = time.time()
+            total_time = (t1-t0) * 1000
+            print(f'finding term time {total_time}')
+            return postings, docIds
+
+def convertToPostings(array: list):
+    ''' takes in list of elements and converts them into pairs of postings'''
+    postings = dict()
+    docIds = []
+    for i in range(0, len(array), 2):
+        # postings.append(Posting(array[i], array[i+1]))
+        postings[array[i]] = array[i+1]
+        docIds.append(array[i])
+    return postings, docIds
+
+def intersectPostings(inverted_lists: list, list_of_docIds: list):
+    ''' intersects (AND) posting list starting from smallest to largest list based off docIds '''
+    t0 = time.time()
+
+    list_of_docIds.sort(key=lambda x:len(x))
+
+    new_inverted_lists = []
+
+    temp = set(list_of_docIds[0])
+    for i in range(1, len(list_of_docIds)):
+        temp = temp & set(list_of_docIds[i])
+    for i in range(len(inverted_lists)):
+        posting = dict()
+        for j in temp:
+            posting[j] = inverted_lists[i][j]
+        new_inverted_lists.append(posting)
+
+    # print(temp)
+    # print(new_inverted_lists)
+    t1 = time.time()
+    total_time = (t1-t0) * 1000
+    print(f'intersection time {total_time}')
+
+    return new_inverted_lists
 
 
 if __name__ == '__main__':
-    index = pickle.load(open('indexfile.txt', 'rb'))
-    docIds = pickle.load(open('docIds.txt', 'rb'))
+    # index = pickle.load(open('indexfile.txt', 'rb'))
+    docIds = pickle.load(open('data/docIds.txt', 'rb'))
+    alphaIndex = pickle.load(open('data/alphaIndex.txt', 'rb'))
+    file_count = count_files(urls_foldername)
+    print(file_count)
 
     query = input('Search: ').lower()
 
     while query != "quit":
-        top5postings = termAtATimeRetrieval(query, index, 5)
+        # start timer 
+        t0 = time.time()
 
-        for posting in top5postings:
-            print(docIds[posting[0]])
+        # get top 5 results from term at a time retrieval
+        toppostings = termAtATimeRetrieval(query, merged_filename, alphaIndex, 10)
+
+        # print out results
+        for posting in toppostings:
+            print(docIds[int(posting[0])])
+
+        # end timer
+        t1 = time.time()
+        total_time = (t1-t0) * 1000
+        print(f"search took {total_time} milliseconds.")
+
+        # prompt user for next query
         query = input('Search: ').lower()
+
+
 
